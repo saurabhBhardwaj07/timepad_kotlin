29c5c9a3f563051b14e6732c8c8dc0e9ce913f20
Saurabh bhardwaj <srvkr0730@gmail.com>
2024-03-18 01:07:12 +0530

    Setup the project

---
+++ app/src/main/java/com/saurabhBhardwaj/timepad/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.saurabhBhardwaj.timepad
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.saurabhBhardwaj.timepad.ui.theme.TimepadTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TimepadTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+            text = "Hello $name!",
+            modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    TimepadTheme {
+        Greeting("Android")
+    }
+}
